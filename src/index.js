dc756bbfe916d0e762fe8f374b325e4963b913a6
Leonardo Schmitt <Leoeyeschmittk@gmail.com>
2020-09-12 21:08:30 -0300

    Commit the first version. Have to solve some issues later

---
+++ src/index.js
@@ -0,0 +1,58 @@
+let musics=['Roses','Ufo','Killer-Queen'];
+
+let authors=['Saint JHN','Sneaky Sound System','Queen']
+
+
+
+ let musicImg=window.document.getElementById('musicImg');
+ let musicName=window.document.getElementById('name');
+ let musicAuthor=window.document.getElementById('author');
+
+ 
+let res;
+ window.addEventListener('load', modifyBg())
+
+   function modifyBg(){
+     setInterval(function(){
+     res= Math.floor(Math.random()*musics.length);
+musicImg.style.backgroundImage=`url("/src/images/${musics[res]}.png")`;
+     },3000)  
+      
+   } 
+
+   window.addEventListener('load', modifyFirstBg())
+   
+   function modifyFirstBg(){
+      res= Math.floor(Math.random()*musics.length);
+musicImg.style.backgroundImage=`url("/src/images/${musics[res]}.png")`;
+   }
+
+
+   function chooseMusic(){
+   window.removeEventListener('load',modifyBg())
+   let chosenMusic= window.document.getElementById(musics[res]); 
+   
+   playMusic(chosenMusic,res)
+
+      
+  } 
+
+   
+
+   
+   function playMusic(chosenMusic,res){ 
+   window.removeEventListener('load',modifyBg())
+
+      musicImg.style.backgroundImage=`url("/src/images/${musics[res]}.png")`; 
+      
+      musicName.innerText=musics[res].split('-').join(' '); 
+      musicAuthor.innerText=authors[res];
+
+      chosenMusic.play();
+
+     
+  } 
+
+
+
+
---
+++ src/test.js
@@ -0,0 +1,10 @@
+let txt='text-texarrt-text-'; 
+let txtNew=txt.split('-').join(' '); 
+console.log(txtNew)
+
+// if(txt.indexOf('-'!=-1)){
+//   for(let i=0;i<11;++i){
+//     txtNew=txt.replace("-"," ");
+//   }
+// }
+
