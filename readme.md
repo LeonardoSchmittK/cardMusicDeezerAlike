95f074aec0f0043d548eab08d6c2596baa7577d6
Leonardo Schmitt <Leoeyeschmittk@gmail.com>
2020-09-12 22:39:39 -0300

    Adding readme.md and some transitions

---
+++ readme.md
@@ -0,0 +1,8 @@
+# :musical_score: Clone Music Card 
+ 
+![screenshot](./screenshot.png)
+
+- [x] Make a interative music card, like Deezer. 
+- [x] Add some musics to it 
+- [x] Use Bootstrap, CSS3, JavaScript and HTML5 
+- [x] Learn something else
--- src/index.js
+++ src/index.js
@@ -7,16 +7,15 @@ let authors=['Saint JHN','Sneaky Sound System','Queen']
  let musicImg=window.document.getElementById('musicImg');
  let musicName=window.document.getElementById('name');
  let musicAuthor=window.document.getElementById('author');
-
- 
 let res;
+
  window.addEventListener('load', modifyBg())
 
    function modifyBg(){
      setInterval(function(){
      res= Math.floor(Math.random()*musics.length);
-musicImg.style.backgroundImage=`url("/src/images/${musics[res]}.png")`;
-     },3000)  
+     musicImg.style.backgroundImage=`url("/src/images/${musics[res]}.png")`;
+     },10000)  
       
    } 
 
@@ -24,7 +23,7 @@ musicImg.style.backgroundImage=`url("/src/images/${musics[res]}.png")`;
    
    function modifyFirstBg(){
       res= Math.floor(Math.random()*musics.length);
-musicImg.style.backgroundImage=`url("/src/images/${musics[res]}.png")`;
+      musicImg.style.backgroundImage=`url("/src/images/${musics[res]}.png")`;
    }
 
 
@@ -47,7 +46,7 @@ musicImg.style.backgroundImage=`url("/src/images/${musics[res]}.png")`;
       
       musicName.innerText=musics[res].split('-').join(' '); 
       musicAuthor.innerText=authors[res];
-
+     
       chosenMusic.play();
 
      
